013b57c99ed15c5648746e59909e3d515b7a41b3
IliaFeldgun <ilia.feldgun@zadarastorage.com>
2025-12-06 21:26:09 +0200

    Rust d3-p2 use p1 in p2 p2 wip

--- src/aoc/three.rs
+++ src/aoc/three.rs
@@ -8,18 +8,45 @@ pub fn main() {
 }
 
 fn run_part(part: i32, input: String) -> Option<Number> {
-    let mut sum: i32 = 0;
+    let mut sum: i128 = 0;
     for line in input.lines() {
-        if let Some(joltage) = get_max_joltage(line) {
+        if part == 1 && let Some(joltage) = get_max_joltage_2(line, 2) {
+            println!("{line}\n{sum}+={joltage}");
+            sum += joltage;
+        } else if let Some(joltage) = get_max_joltage_2(line, 12) {
+            println!("{line}\n{sum}+={joltage}");
             sum += joltage;
         } else {
             return None;
         }
     }
-    Some(Number::Int32(sum))
+    Some(Number::Int128(sum))
+}
+fn get_max_joltage_2(line: &str, digits: usize) -> Option<i128> {
+    let mut digit_collection: Vec<char> = vec!['0'; digits];
+    let max_line_index = line.len() - digits + 1;
+    for digit_index in 0..(digits - 1) {
+        for (line_index, current_char) in line.chars().enumerate() {
+            // let (max_first, max_second) = get_max_joltage_1_chars(&line[line_index..]);
+            let (max_first, max_second) = get_max_joltage_1_chars(line);
+            digit_collection[digit_index] = max_first;
+            digit_collection[digit_index + 1] = max_second;
+        }
+    }
+    let mut result = String::from("");
+    for digit in digit_collection {
+        result = format!("{result}{digit}")
+    }
+    match result.trim().parse::<i128>() {
+        Err(e) => {
+            eprintln!("Failed to parse {result}: {e}");
+            None
+        }
+        Ok(number) => Some(number),
+    }
 }
 
-fn get_max_joltage(line: &str) -> Option<i32> {
+fn get_max_joltage_1_chars(line: &str) -> (char, char) {
     let mut max_first: char = '0';
     let mut max_second: char = '0';
     for (index, current_char) in line.chars().enumerate() {
@@ -30,13 +57,5 @@ fn get_max_joltage(line: &str) -> Option<i32> {
             max_second = current_char;
         }
     }
-    let result = format!("{max_first}{max_second}");
-
-    match result.trim().parse::<i32>() {
-        Err(e) => {
-            eprintln!("Failed to parse {result}: {e}");
-            None
-        }
-        Ok(number) => Some(number),
-    }
+    (max_first, max_second)
 }
