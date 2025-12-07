34ed5ebd8828752228242960efe6f18a901c0a18
IliaFeldgun <ilia.feldgun@zadarastorage.com>
2025-12-08 00:45:02 +0200

    Rust d5-p2

--- src/aoc/five.rs
+++ src/aoc/five.rs
@@ -11,6 +11,62 @@ pub fn main() {
 }
 
 fn run_part(part: i32, input: String) -> Option<Number> {
+    if part == 1 && let Some(count) = run_part_1(input.clone()) {
+        return Some(Number::Int32(count));
+    } else if let Some(count) = run_part_2(input.clone()) {
+        return Some(Number::Int128(count));
+    }
+    None
+}
+
+fn run_part_2(input: String) -> Option<i128> {
+    let mut ranges: Vec<(&str, &str)> = Vec::new();
+    let mut ids: Vec<&str> = Vec::new();
+    let mut good_ranges: HashMap<i128, i128> = HashMap::new();
+    for line in input.lines() {
+        if line.contains("-") && let Some(ids) = line.split_once("-") {
+            ranges.push(ids);
+        } else if !line.trim().is_empty() {
+            ids.push(line.trim());
+        }
+    }
+    for (start, end) in ranges {
+        if let Some(istart) = parse_id(start) && let Some(iend) = parse_id(end) {
+            let mut new_start = istart;
+            let mut new_end = iend;
+            let mut remove_indices: Vec<i128> = Vec::new();
+            for (good_start, good_end) in good_ranges.clone() {
+                if new_end < good_start - 1 {
+                    continue;
+                }
+                if new_start > good_end + 1 {
+                    continue;
+                }
+                if new_end <= good_end {
+                    new_end = good_end;
+                }
+                if good_start <= new_start {
+                    new_start = good_start;
+                } else {
+                    remove_indices.push(good_start);
+                }
+            }
+            for i in remove_indices {
+                good_ranges.remove(&i);
+            }
+            good_ranges.insert(new_start, new_end);
+        } else {
+            eprintln!("Failed to parse range {start}-{end}");
+            return None;
+        }
+    }
+    let mut count: i128 = 0;
+    for (good_start, good_end) in good_ranges.clone() {
+        count += good_end - good_start + 1
+    }
+    Some(count)
+}
+fn run_part_1(input: String) -> Option<i32> {
     let mut ranges: Vec<(&str, &str)> = Vec::new();
     let mut ids: Vec<&str> = Vec::new();
     let mut good_ids: HashMap<i128, bool> = HashMap::new();
@@ -26,15 +82,14 @@ fn run_part(part: i32, input: String) -> Option<Number> {
             for id in ids.clone() {
                 if let Some(iid) = parse_id(id) {
                     if iid <= iend && iid >= istart {
-                        println!("{iid}");
                         good_ids.insert(iid, true);
                     }
-                }
+        }
             }
         } else {
             eprintln!("Failed to parse range {start}-{end}");
             return None;
         }
     }
-    Some(Number::Int32(good_ids.len() as i32))
+    Some(good_ids.len() as i32)
 }
