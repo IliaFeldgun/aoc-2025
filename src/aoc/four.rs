03847ea1e76124bf15b1d590cc62c572d9ef3b3e
IliaFeldgun <ilia.feldgun@zadarastorage.com>
2025-12-08 00:45:02 +0200

    Rust d4-p2

--- src/aoc/four.rs
+++ src/aoc/four.rs
@@ -9,7 +9,7 @@ pub fn main() {
 }
 
 fn run_part(part: i32, input: String) -> Option<Number> {
-    if part == 1 && let Some(rolls) = get_rolls(input, part) {
+    if let Some(rolls) = get_rolls(input, part) {
         println!("{rolls}");
         Some(Number::Int32(rolls))
     } else {
@@ -19,38 +19,41 @@ fn run_part(part: i32, input: String) -> Option<Number> {
 
 fn get_rolls(input: String, part: i32) -> Option<i32> {
     let mut mat = string_to_mat(&input);
-    let matt = mat.clone();
     echo_mat(&mat);
-    let mut rolls = 0;
-    for (i, row) in mat.iter_mut().enumerate() {
-        for (j, cell) in row.iter_mut().enumerate() {
-            if *cell == '@' {
-                let sub_mat = square_submat(&matt, i, j, 3);
-                println!("{i},{j}");
-                echo_mat(&sub_mat);
-                let mut sub_rolls = 0;
-                for (si, sr) in sub_mat.iter().enumerate() {
-                    for (sj, sc) in sr.iter().enumerate() {
-                        if *sc == '@' || *sc == 'X' {
-                            sub_rolls += 1;
+    let mut rolls = -1;
+    let mut new_rolls = 0;
+    while new_rolls != rolls {
+        let matt = mat.clone();
+        rolls = new_rolls;
+        for (i, row) in mat.iter_mut().enumerate() {
+            for (j, cell) in row.iter_mut().enumerate() {
+                if *cell == '@' {
+                    let sub_mat = square_submat(&matt, i, j, 3);
+                    echo_mat(&sub_mat);
+                    let mut sub_rolls = 0;
+                    for (si, sr) in sub_mat.iter().enumerate() {
+                        for (sj, sc) in sr.iter().enumerate() {
+                            if *sc == '@' || *sc == 'X' {
+                                sub_rolls += 1;
+                            }
                         }
                     }
-                }
-                if sub_rolls < 5 {
-                    *cell = 'X';
-                    println!("OK")
-                } else {
-                    println!("NOPE")
+                    if sub_rolls < 5 {
+                        *cell = 'X';
+                    }
                 }
             }
         }
-    }
-    echo_mat(&mat);
-    for (i, row) in mat.iter().enumerate() {
-        for (j, cell) in row.iter().enumerate() {
-            if *cell == 'X' {
-                rolls += 1;
+        for (i, row) in mat.iter_mut().enumerate() {
+            for (j, cell) in row.iter_mut().enumerate() {
+                if *cell == 'X' {
+                    *cell = 'x';
+                    new_rolls += 1;
+                }
             }
+        }
+        if part == 1 {
+            return Some(new_rolls);
         }
     }
     Some(rolls)
